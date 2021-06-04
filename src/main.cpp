238eeb14c02edbd0f52be0680fe392d7d7d9d4f9
Trond H Emaus <trondhe@gmail.com>
2021-06-04 23:53:02 +0200

    make it compile

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 // #include "lib.rs.h"
 #include <iostream>
 
+extern ::std::int32_t rusty_integer() noexcept;
+
 extern "C" {
 
 int32_t rusty_c_integer();
@@ -8,7 +10,7 @@ int32_t rusty_c_integer();
 
 int main() {
   std::cout << "Hello!\n";
-  // std::cout << rusty_integer();
+  // std::cout << rusty_integer(); // Uncomment here to get the linker error
   std::cout << "direct extern c function from rust with value: "
             << rusty_c_integer() << "\n";
   return 0;
