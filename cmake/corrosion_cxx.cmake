17ddad27f0d9987fe09dfae1a7e8e51088d8844a
Trond H Emaus <trondhe@gmail.com>
2021-06-04 23:02:50 +0200

    add outcommented raw cxxbridge cmake code

--- cmake/corrosion_cxx.cmake
+++ cmake/corrosion_cxx.cmake
@@ -12,4 +12,37 @@ function(add_rust_lib path)
     endif()
 
     corrosion_import_crate(MANIFEST_PATH "${path}/Cargo.toml")
+
+    # cmake_path(GET path STEM RUSTLIB_PROJECTFOLDER_NAME)
+
+    # set(RUST_TARGET_TRIPLE "x86_64-pc-windows-msvc")
+
+    # set(CXXBRIDGE_BINARY_FOLDER ${CMAKE_BINARY_DIR}/cargo/build/${RUST_TARGET_TRIPLE}/cxxbridge) 
+
+    # set(CXX_COMMON_HEADER ${CXXBRIDGE_BINARY_FOLDER}/rust/cxx.h)
+    # set(CXX_BINDING_HEADER ${CXXBRIDGE_BINARY_FOLDER}/rusty_code/src/lib.rs.h)
+    # set(CXX_BINDING_SOURCE ${CXXBRIDGE_BINARY_FOLDER}/rusty_code/src/lib.rs.cc)
+    
+    # set(CXX_BINDING_INCLUDE_DIR ${CXXBRIDGE_BINARY_FOLDER})
+
+    # add_custom_command(
+    #     OUTPUT 
+    #         ${CXX_COMMON_HEADER}
+    #         ${CXX_BINDING_HEADER}
+    #         ${CXX_BINDING_SOURCE}
+    #     DEPENDS rusty_code
+    # )
+
+    # add_library(rust_code_cxxbridge)
+    # target_sources(rust_code_cxxbridge
+    #     PUBLIC
+    #         ${CXX_COMMON_HEADER}
+    #         ${CXX_BINDING_HEADER}
+    #         ${CXX_BINDING_SOURCE}
+    # )
+    # target_include_directories(rust_code_cxxbridge
+    #     PUBLIC
+    #         ${CXX_BINDING_INCLUDE_DIR}
+    # )
+    
 endfunction(add_rust_lib)
