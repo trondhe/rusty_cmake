d2367ecc6bcbe8e1a0da38c36ca64d67f208317d
Trond H Emaus <trondhe@gmail.com>
2021-06-04 23:04:59 +0200

    add wip main code to test behaviour

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
+// #include "lib.rs.h"
 #include <iostream>
 
+extern "C" {
+
+int32_t rusty_c_integer();
+}
+
 int main() {
-  std::cout << "Hello!";
+  std::cout << "Hello!\n";
+  std::cout << rusty_integer();
+  std::cout << "direct extern c function from rust with value: "
+            << rusty_c_integer() << "\n";
   return 0;
 }
