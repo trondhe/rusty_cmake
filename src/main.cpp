edd100a0461a8927bdb618fb7b5e0157a8151d80
Trond H Emaus <the@DESKTOP-O97M939.localdomain>
2021-06-05 00:08:04 +0200

    add newline

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int32_t rusty_c_integer();
 
 int main() {
   std::cout << "Hello!\n";
-  // std::cout << rusty_integer(); // Uncomment here to get the linker error
+  std::cout << rusty_integer() << "\n"; // Uncomment here to get the linker error
   std::cout << "direct extern c function from rust with value: "
             << rusty_c_integer() << "\n";
   return 0;
